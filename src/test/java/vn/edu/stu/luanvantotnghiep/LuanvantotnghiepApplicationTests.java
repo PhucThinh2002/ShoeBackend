d4c8c56b32feeab6f5976b20889f01dca88ba4ac
NQH1611 <quochuy.140101@gmail.com>
2023-04-20 21:41:33 +0700

    first commit

---
+++ src/test/java/vn/edu/stu/luanvantotnghiep/LuanvantotnghiepApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.stu.luanvantotnghiep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuanvantotnghiepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
