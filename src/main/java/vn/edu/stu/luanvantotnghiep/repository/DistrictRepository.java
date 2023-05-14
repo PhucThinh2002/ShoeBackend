e68718f4869048538f0ffa29115178ab901eecc1
NQH1611 <quochuy.140101@gmail.com>
2023-05-14 15:15:14 +0700

    update district repo

--- src/main/java/vn/edu/stu/luanvantotnghiep/repository/DistrictRepository.java
+++ src/main/java/vn/edu/stu/luanvantotnghiep/repository/DistrictRepository.java
@@ -1,10 +1,13 @@
 package vn.edu.stu.luanvantotnghiep.repository;
 import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import vn.edu.stu.luanvantotnghiep.model.District;
 
 
 public interface DistrictRepository extends JpaRepository<District, Long>{
-    List<District> findByProvince(int province);
+    @Query(value = "SELECT * FROM DISTRICT WHERE PROVINCE = :province", nativeQuery = true)
+    List<District> findByProvince(@Param("province") int province);
 }
