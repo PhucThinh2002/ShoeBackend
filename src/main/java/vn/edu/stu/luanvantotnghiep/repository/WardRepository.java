4020a3c7427cfe0be0b498588e3907726de1ca84
NQH1611 <quochuy.140101@gmail.com>
2023-06-09 21:43:29 +0700

    update repo ward

--- src/main/java/vn/edu/stu/luanvantotnghiep/repository/WardRepository.java
+++ src/main/java/vn/edu/stu/luanvantotnghiep/repository/WardRepository.java
@@ -1,8 +1,11 @@
 package vn.edu.stu.luanvantotnghiep.repository;
 import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import vn.edu.stu.luanvantotnghiep.model.Ward;
 public interface WardRepository extends JpaRepository<Ward, Long>{
-    List<Ward> findByDistrict(int district);
+    @Query(value = "SELECT * FROM ward WHERE district = :district", nativeQuery = true)
+    List<Ward> findByDistrict(@Param("distrcit")int district);
 }
