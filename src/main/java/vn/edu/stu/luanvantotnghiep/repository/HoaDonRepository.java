1756b088f5197ef2a6d4e481c1907b5ff8e4f19b
NQH1611 <quochuy.140101@gmail.com>
2023-06-13 20:30:19 +0700

    update authorise

--- src/main/java/vn/edu/stu/luanvantotnghiep/repository/HoaDonRepository.java
+++ src/main/java/vn/edu/stu/luanvantotnghiep/repository/HoaDonRepository.java
@@ -1,9 +1,16 @@
 package vn.edu.stu.luanvantotnghiep.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import vn.edu.stu.luanvantotnghiep.model.HoaDon;
 
 public interface HoaDonRepository extends JpaRepository<HoaDon, Integer>{
-    
+    @Query(value = "select * from hoa_don where trang_thai = 1", nativeQuery = true)
+    List<HoaDon> findAllHoaDonActive();
+    @Query(value = "select * from hoa_don where khach_hang_id = :khachhang and trang_thai = 1", nativeQuery = true)
+    List<HoaDon> findAllHoaDonByKhachHang(@Param("khachhang") Integer khachHang);
 }
--- src/main/java/vn/edu/stu/luanvantotnghiep/repository/SanPhamRepository.java
+++ src/main/java/vn/edu/stu/luanvantotnghiep/repository/SanPhamRepository.java
@@ -1,9 +1,16 @@
 package vn.edu.stu.luanvantotnghiep.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import vn.edu.stu.luanvantotnghiep.model.SanPham;
 
 public interface SanPhamRepository extends JpaRepository<SanPham, Integer>{
-    
+    @Query(value = "select * from san_pham where trang_thai = 1 and danh_muc = :danhmuc", nativeQuery = true)
+    List<SanPham> findSanPhamByDanhMucActive(@Param("danhmuc") Integer danhMuc);
+    @Query(value = "select * from san_pham where trang_thai = 1 and nha_san_xuat = :nhasanxuat", nativeQuery = true)
+    List<SanPham> findSanPhamByNhaSanXuatActive(@Param("nhasanxuat") Integer nhaSanXuat);
 }
